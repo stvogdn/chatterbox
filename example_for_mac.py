aa1607566147c2577bcae120c4d82d2f80ba6a45
Steve Ogden <stvogdn@gmail.com>
2026-06-27 13:25:01 -0700

    minor changes for Apple silicon

--- example_for_mac.py
+++ example_for_mac.py
@@ -18,11 +18,11 @@ model = ChatterboxTTS.from_pretrained(device=device)
 text = "Today is the day. I want to move like a titan at dawn, sweat like a god forging lightning. No more excuses. From now on, my mornings will be temples of discipline. I am going to work out like the gods… every damn day."
 
 # If you want to synthesize with a different voice, specify the audio prompt
-AUDIO_PROMPT_PATH = "YOUR_FILE.wav"
+AUDIO_PROMPT_PATH = "steve.wav"
 wav = model.generate(
     text, 
     audio_prompt_path=AUDIO_PROMPT_PATH,
-    exaggeration=2.0,
+    exaggeration=0.5,
     cfg_weight=0.5
     )
 ta.save("test-2.wav", wav, model.sr)
--- gradio_tts_app.py
+++ gradio_tts_app.py
@@ -5,7 +5,7 @@ import gradio as gr
 from chatterbox.tts import ChatterboxTTS
 
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
 
 
 def set_seed(seed: int):
